56b25cd75684c926c80ef25894a484ee057bb852
BaiDom <dombaily88@gmail.com>
2022-12-06 14:21:59 +0000

    few more tweaks to syling on single review, margin around category etc

--- src/components/Single-review.jsx
+++ src/components/Single-review.jsx
@@ -20,18 +20,18 @@ const SingleReview = () => {
 
   return (
     <div className="single-review-card">
-      <p id="review-id">Review id: {review.review_id}</p>
+      <p id="single-review-id">Review id: {review.review_id}</p>
       <p id="review-title">{review.title}</p>
       <p id="review-designer">Designed by: {review.designer}</p>
       <img src={review.review_img_url} id="review-img" alt={review.title}></img>
       <div id="review-card-internal">
         <p id="review-body">{review.review_body}</p>
+        <p id="single-review-category">Category: {review.category}</p>
       </div>
-      <div className="review-grid-container">
+      <div className="single-review-grid-container">
         <p id="comment-count">No. of comments: {review.comment_count}</p>
         <p id="votes">Votes: {review.votes}</p>
       </div>
-      <p id="review-category">Category: {review.category}</p>
     </div>
   );
 };
