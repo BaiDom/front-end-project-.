2f4152b5ec234f3276f6f720522f49fb2b5887e5
BaiDom <dombaily88@gmail.com>
2022-12-06 10:53:58 +0000

    loading text added

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -4,44 +4,52 @@ import { getReviews } from "../api";
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getReviews().then((reviews) => {
       setReviews(reviews);
+      setIsLoading(false);
     });
   }, []);
 
   return (
     <div>
-      <ul id="review-card-ul">
-        {reviews.map((review) => {
-          return (
-            <article>
-              <li key={review.id} className="review-card">
-                <p id="review-id">Review id: {review.review_id}</p>
-                <p id="review-title">{review.title}</p>
-                <p id="review-designer">Designed by: {review.designer}</p>
-                <img
-                  src={review.review_img_url}
-                  id="review-img"
-                  alt={review.title}
-                ></img>
-                <p id="review-category">Category: {review.category}</p>
-                <div id="review-card-internal">
-                  <p id="review-body">{review.review_body}</p>
-                </div>
-
-                <div className="review-grid-container">
-                  <p id="comment-count">
-                    No. of comments: {review.comment_count}
-                  </p>
-                  <p id="votes">Votes: {review.votes}</p>
-                </div>
-              </li>
-            </article>
-          );
-        })}
-      </ul>
+      {isLoading ? (
+        <div>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <ul id="review-card-ul">
+          {reviews.map((review) => {
+            return (
+              <article>
+                <li key={review.id} className="review-card">
+                  <p id="review-id">Review id: {review.review_id}</p>
+                  <p id="review-title">{review.title}</p>
+                  <p id="review-designer">Designed by: {review.designer}</p>
+                  <img
+                    src={review.review_img_url}
+                    id="review-img"
+                    alt={review.title}
+                  ></img>
+                  <p id="review-category">Category: {review.category}</p>
+                  <div id="review-card-internal">
+                    <p id="review-body">{review.review_body}</p>
+                  </div>
+                  <div className="review-grid-container">
+                    <p id="comment-count">
+                      No. of comments: {review.comment_count}
+                    </p>
+                    <p id="votes">Votes: {review.votes}</p>
+                  </div>
+                </li>
+              </article>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
