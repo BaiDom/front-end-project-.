524d48e6610fd0151bdff1acfc3adaf44d0010a8
BaiDom <dombaily88@gmail.com>
2023-11-12 12:00:17 +0000

    started making changes to styling and UI to add to portfolio site

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,16 +2,18 @@ import "../Landing.css";
 
 const Landing = () => {
   return (
-    <div className="page-container">
-      <article id="landing-body">
-        <p id="landing-p1">
-          This was a first attempt at building a full-stack app during the
-          Northcoders course, I developer an SQL database back-end and API to
-          access it using this front end interface, I will continue to make
-          uopdates to the UI when I can!
-        </p>
-      </article>
-    </div>
+    <section className="landing-section">
+      <div className="page-container">
+        <article id="landing-body">
+          <p id="landing-p1">
+            This was a first attempt at building a full-stack app during the
+            Northcoders course, I developer an SQL database back-end and API to
+            access it using this front end interface, I will continue to make
+            updates to the UI when I can!
+          </p>
+        </article>
+      </div>
+    </section>
   );
 };
 
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -36,34 +36,39 @@ const Reviews = ({ currCategory, setCurrCategory, sortBy, setSortBy }) => {
         </div>
       ) : (
         <section className="reviews-section flex">
-          {reviews.map((review) => {
-            return (
-              <article key={`${review.review_id}`} className="review-card">
-                <p id="review-title">{review.title}</p>
-                <div id="review-card-internal">
-                  <Link
-                    to={`/reviews/${review.review_id}`}
-                    className="review-card-link"
-                    id={`review-card-link-${review.review_id}`}
-                  >
-                    <p id="review-designer">Designed by: {review.designer}</p>
-                    <p id="review-category">Category: {review.category}</p>
-                    <img
-                      src={review.review_img_url}
-                      id="review-img"
-                      alt={review.title}
-                    ></img>
-                    <p id="click">Click for full review!</p>
-                  </Link>
-                  <p id="comment-count">{review.comment_count} Comments</p>
+          <div className="reviews-cont">
+            {reviews.map((review) => {
+              return (
+                <article
+                  key={`${review.review_id}`}
+                  className="review-card flex"
+                >
+                  <div id="review-card-internal">
+                    <Link
+                      to={`/reviews/${review.review_id}`}
+                      className="review-card-link"
+                      id={`review-card-link-${review.review_id}`}
+                    >
+                      <p id="review-category">-&nbsp;{review.category}</p>
+                      <p id="review-title">{review.title}</p>
+                      <p id="review-designer">Designed by: {review.designer}</p>
+                      <img
+                        src={review.review_img_url}
+                        id="review-img"
+                        alt={review.title}
+                      ></img>
+                      <p id="click">Click for full review!</p>
+                    </Link>
+                    <p id="comment-count">{review.comment_count} Comments</p>
 
-                  <div id="review-vote-container">
-                    <p id="votes">Votes for this review: {review.votes}</p>
+                    <div id="review-vote-container">
+                      <p id="votes">Votes for this review: {review.votes}</p>
+                    </div>
                   </div>
-                </div>
-              </article>
-            );
-          })}
+                </article>
+              );
+            })}
+          </div>
         </section>
       )}
     </div>
